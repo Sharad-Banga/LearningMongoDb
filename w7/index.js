937239c4665d762d3fa6b5214c08456862004083
Sharad Banga <sharadiit03@gmail.com>
2025-01-06 14:16:07 +0530

    first commit

---
+++ w7/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const app = express();
+
+app.post('/signup',(req,res)=>{
+
+})
+
+
+app.post('/signin',(req,res)=>{
+  
+})
+
+
+app.post('/todo',(req,res)=>{
+  
+})
+
+
+app.post('/todos',(req,res)=>{
+  
+})
+
+app.listen(3000);
