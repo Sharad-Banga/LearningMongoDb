ccab21214b91c3979fad64f49738c6cc8665807d
Sharad Banga <sharadiit03@gmail.com>
2025-01-06 15:25:18 +0530

    some updation

--- w7/db.js
+++ w7/db.js
@@ -1,10 +1,31 @@
 
-import mongoose from 'mongoose';
+import mongoose, { model } from 'mongoose';
 
 const Schema = mongoose.Schema;
 
+//we have two collection , user and todo
+//user contains all the user that sign up
+//todo contains todos 
+
+//this is schema of user db
 const User = new Schema({
   email : String,
   password : String,
   name : String
 })
+
+//this is schema of todo db
+const todo = new Schema({
+  title : String,
+  done : Boolean,
+  userId : ObjectId // user id of person(already present in user db) whose todo it is
+})
+
+//creating models 
+const UserModel = mongoose.model('users',User);
+const TodoModel = mongoose.model('todos',todo);
+
+model.exports = {
+  UserModel : UserModel,
+  TodoModel : TodoModel
+}
