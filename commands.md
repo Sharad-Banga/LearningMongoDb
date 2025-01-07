a1fd490545462b258c7ac76a6e6d13bfd1a7b29c
Sharad Banga <sharadiit03@gmail.com>
2025-01-07 08:50:55 +0530

    some updations

---
+++ commands.md
@@ -0,0 +1,3 @@
+git add .
+git commit -m "some updations"
+git push origin main
--- w7/index.js
+++ w7/index.js
@@ -1,14 +1,51 @@
 import express from 'express';
+import { UserModel, TodoModel } from './db';
 
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "sharad12345";
+app.use(express.json());
 const app = express();
 
-app.post('/signup',(req,res)=>{
+app.post('/signup',async (req,res)=>{
+  const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
+
+  await UserModel.insert({
+    email : email,
+    password : password,
+    name : name
+  })
+
+  res.send({
+    message : "you are signed up"
+  })
 
 })
 
 
 app.post('/signin',(req,res)=>{
-  
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = UserModel.findOne({
+    email : email,
+    password : password
+  })
+
+  if(user){
+    const token = "";
+    res.json({
+      token : token
+    })
+  }
+  else{
+    res.status(403).json({
+      message : "Incorrect password"
+    })
+  }
+
 })
 
 
